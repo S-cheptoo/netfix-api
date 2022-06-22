401d1fb5057772ada706ec33d10a61e5f871f2c1
Sandra <cheptoosandra.t@gmail.com>
2022-06-22 23:17:47 +0300

    Routes

--- index.js
+++ index.js
@@ -2,16 +2,22 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const authRoute = require("./routes/auth");
 
 dotenv.config();
 
 mongoose
     .connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
-    useUnifiedTopology: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
     })
     .then(() => console.log("DB Connection Successful!"))
     .catch((err => console.log(err)));
+
+app.use(express.json());
+
+app.use("api/auth", authRoute);
 
 
 app.listen(8800, () => {
